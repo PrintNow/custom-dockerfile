b7c5972505e46a79a55ad1b6f05a434466a68ef2
Chuwen <i@nowtime.cc>
2022-07-30 13:11:43 +0800

    feat: 添加如果是 dev 就安装调试相关的扩展

--- phpswoole/depoly.sh
+++ phpswoole/depoly.sh
@@ -21,17 +21,24 @@ echo_b() {
 docker_build_push() {
   [ $# -ne 1 ] && return 0
 
+  http_proxy_url="http://127.0.0.1:7890"
   swoole_version="$1"
-#  http_proxy_url="http://127.0.0.1:7890"
-  php_extensions="csv inotify"
+  php_extensions="csv"
 
   echo_b "开始构建镜像：$swoole_version"
+
+  # 如果包含 dev 就去安装开发相关的东西
+  if [[ $swoole_version =~ "-dev" ]]; then
+    php_extensions="inotify xdebug $php_extensions"
+    echo_b "@包含 dev，安装这些扩展：${php_extensions}"
+  fi
+
   docker build \
-#    --build-arg HTTP_ROXY="$http_proxy_url" \
-#    --build-arg HTTPS_ROXY="$http_proxy_url" \
     --build-arg PHP_EXTENSIONS="$php_extensions" \
     --build-arg PHPSWOOLE_VERSION="$swoole_version" \
     -t shine09/php:"$swoole_version" .
+  # --build-arg HTTP_ROXY="$http_proxy_url" \
+  # --build-arg HTTPS_ROXY="$http_proxy_url" \
 
   # 捕获是否异常
   [ $? -eq 0 ] || exit 1
@@ -45,6 +52,9 @@ docker_build_push() {
 # 定义数组变量
 declare -a PHPSWOOLE_VERSION_ARRAY
 PHPSWOOLE_VERSION_ARRAY=(
+  "php8.1-alpine"
+  "php8.1"
+  "php8.1-dev"
   "4.8-php8.1-alpine"
   "4.8-php8.1"
   "4.8-php8.1-dev"
